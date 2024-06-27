c3a722ae54290f0b3f8a6717a1885d5492c4407d
LtMilligan <gioele.miscia@gmail.com>
2024-06-27 19:16:08 +0200

    [UPD] Added css style

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
-// Creo un FOR per andare a creare un ciclo di incremento unitario da 1 a 100
+const board = document.querySelector('.board');
+
+
 for (i = 1; i <= 100; i++) {
-    let number = i
-    // Creo un IF per andare a cercare i numeri divisibili per 3 per andare a sostituire il numero con "Fizz"
-    if (i % 3 == 0) {
-        i = 'Fizz'
+    const content = document.createElement('div');   
+    content.classList.add('content');
+    
+    if (i % 3 == 0 && i % 5 == 0) {
+        content.innerText = 'FizzBuzz'
+        content.classList.add('fizzbuzz')
+    }else if (i % 5 == 0) {
+        content.innerText = 'Buzz'
+        content.classList.add('buzz')
+    }else if (i % 3 == 0) {
+        content.innerText = 'Fizz'
+        content.classList.add('fizz')
+    }else {
+        content.innerText = `${i}`
+        content.classList.add('generic')
     }
-    // Creo un IF per andare a cercare i numeri divisibili per 5 per andare a sostituire il numero con "Buzz"
-    else if (i % 5 == 0) {
-        i = 'Buzz'
-    }
-    // Creo un IF per andare a cercare i numeri divisibili sia per 3 che per 5 per andare a sostituire il numero con "FizzBuzz"
-    else if (i % 3 == 0 && i % 5 == 0) {
-        i = 'FizzBuzz'
-    }
+
+    board.append(content)
 }
