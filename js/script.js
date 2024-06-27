65c894e87e08ceafaaf32408db13352eb0a47739
LtMilligan <gioele.miscia@gmail.com>
2024-06-27 16:48:40 +0200

    [UPD] First IF completed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 // Creo un FOR per andare a creare un ciclo di incremento unitario da 1 a 100
 for (i = 1, i <= 100, i++) {
-    
+    // Creo un IF per andare a cercare i numeri divisibili per 3 per andare a sostituire il numero con "Fizz"
+    if (i % 3 == 0) {
+        i = 'Fizz'
+    }
 }
